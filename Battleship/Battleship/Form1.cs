91988d686384220b0adeb0683780ecce9ccc00c0
Tyler Tong <tylertong519@gmail.com>
2024-08-17 17:32:04 -0700

    asdjf;lkaj;sdf

---
+++ Battleship/Battleship/Form1.cs
@@ -0,0 +1,25 @@
+namespace Battleship
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
